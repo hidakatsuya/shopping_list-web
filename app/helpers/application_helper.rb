6bcc516ba1f2fd7db6acb2e7294c3da0a512aff8
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-06-02 00:48:01 +0900

    add style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
-  def render_turbo_stream_flash_messages
-    turbo_stream.prepend "flash", partial: "layouts/flash"
-  end
-
-  def form_error_notification(object)
-    if object.errors.any?
-      tag.div class: "error-message" do
-        object.errors.full_messages.to_sentence.capitalize
-      end
-    end
+  def form_error_notification(model)
+    render partial: 'layouts/error_notification', locals: { model: model } if model.errors.any?
   end
 end
