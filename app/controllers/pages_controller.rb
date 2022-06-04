b8b3e25ec6c41d051f366020722261a8d80698bb
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-06-04 17:23:58 +0900

    add home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def home
+    if user_signed_in?
+      redirect_to items_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :users
 
   resources :items, except: [:show] do
