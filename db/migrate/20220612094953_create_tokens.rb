999c4f740e55ba0ef2d40860bdac7a299c4add80
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-06-13 21:24:38 +0900

    add index to value of tokens

--- db/migrate/20220612094953_create_tokens.rb
+++ db/migrate/20220612094953_create_tokens.rb
@@ -6,5 +6,6 @@ class CreateTokens < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    add_index :tokens, :value
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_12_094953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tokens_on_user_id"
+    t.index ["value"], name: "index_tokens_on_value"
   end
 
   create_table "users", force: :cascade do |t|
