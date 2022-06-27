9b50eb812683b5600f9f93735608c53661dca945
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-06-28 00:29:11 +0900

    donot use Validatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :items, dependent: :destroy
   has_one :token, dependent: :destroy
 
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: true, allow_blank: true },
+                    format: { with: ::Devise.email_regexp, allow_blank: true }
+
   class << self
-    def from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
+    def from_omniauth(provider:, uid:, email:, create_user: false)
+      attributes = { provider: provider, uid: uid }
+
+      user = find_by(attributes)
+      return user if user
+
+      if create_user
+        create(attributes.merge(email: email))
+      else
+        nil
       end
     end
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,4 +3,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test '.from_omniauth with registered user' do
+    user = users(:user_a)
+
+    assert_equal user, ::User.from_omniauth(provider: 'google_oauth2',
+                                            uid: user.uid,
+                                            email: user.email)
+  end
+
+  test '.from_omniauth with new user' do
+    user = nil
+
+    assert_no_changes ->{ User.count } do
+      user = ::User.from_omniauth(provider: 'google_oauth2',
+                                  uid: 9999,
+                                  email: 'new_user@example.com')
+    end
+    assert_nil user
+  end
+
+  test '.from_omniauth with new user and create_user: true option' do
+    auth_attrs = {
+      provider: 'google_oauth2',
+      uid: 9999,
+      email: 'new_user@example.com'
+    }
+    user = ::User.from_omniauth(**auth_attrs, create_user: true)
+    assert ::User.where(auth_attrs.slice(:provider, :uid)).exists?
+  end
 end
