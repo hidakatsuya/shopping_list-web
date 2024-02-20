44b6ea0427d5824c4272d78dd57f0a84778bf235
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-02-21 01:57:22 +0900

    Fixed unintended behavior where db:setup and db:prepare would execute db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-puts "\n== Seeding the database with fixtures =="
-system('bin/rails db:fixtures:load')
